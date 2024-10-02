de13ab0b23576d2ae682a602e1899e6b70d603c3
Hishaam Namooya <n.hishaam@gmail.com>
2024-10-02 23:59:36 +0400

    Add event handler to update link url

---
+++ Arcwave.Events/Constant.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcwave.Events
+{
+    public static class Constant
+    {
+        public static class TemplateId
+        {
+            public static readonly ID ApplicationShortcutTemplateId = new ID("{72450C9C-98C4-4117-88B7-573110C7E0C0}");
+        }
+
+        public static class FieldId
+        {
+            public static readonly ID ApplicationFieldId = new ID("{8A1AC797-0304-4C50-AAAC-4D9DBBFF435F}");
+        }
+
+        public static class Pattern
+        {
+            public static readonly string UrlPattern = @"url=""([^""]*)""";
+
+            public static readonly string IdPattern = @"id=""{([^}]*)}""";
+        }
+    }
+}
---
+++ Arcwave.Events/Handlers/ApplicationShortcut.cs
@@ -0,0 +1,66 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Events;
+using Sitecore.StringExtensions;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Arcwave.Events.Handlers
+{
+    public class ApplicationShortcut
+    {
+        private Database CoreDatabase
+        {
+            get
+            {
+                return Factory.GetDatabase("core");
+            }
+        }
+
+        public void OnItemSaving(object sender, EventArgs args)
+        {
+            var eventArgs = args as SitecoreEventArgs;
+
+            var updatedItem = eventArgs.Parameters[0] as Item;
+
+            if (this.ShouldSkipItem(updatedItem))
+            {
+                return;
+            }
+
+            var itemChanges = eventArgs.Parameters[1] as ItemChanges;
+
+            if (itemChanges.FieldChanges.Contains(Constant.FieldId.ApplicationFieldId)) //Application Field ID
+            {
+                var applicationLinkNewValue = itemChanges.FieldChanges[Constant.FieldId.ApplicationFieldId].Value;
+
+                if (!applicationLinkNewValue.IsNullOrEmpty())
+                {
+                    Match match = Regex.Match(applicationLinkNewValue, Constant.Pattern.IdPattern);
+                    
+                    if (match.Success)
+                    {
+                        var idValue = match.Groups[1].Value;
+
+                        var applicationItem = this.CoreDatabase.GetItem(new ID(idValue));
+
+                        if(applicationItem != null)
+                        {
+                            var path = applicationItem.Paths.ContentPath;
+
+                            applicationLinkNewValue = Regex.Replace(applicationLinkNewValue, Constant.Pattern.IdPattern, $"url=\"{path}\"");
+
+                            itemChanges.SetFieldValue(updatedItem.Fields[Constant.FieldId.ApplicationFieldId], applicationLinkNewValue);
+                        }
+                    }                    
+                }
+            }
+        }
+
+        private bool ShouldSkipItem(Item item)
+        {
+            return item == null || item.Database.Name.ToLower() != "core" || item.TemplateID != Constant.TemplateId.ApplicationShortcutTemplateId;
+        }
+    }
+}
